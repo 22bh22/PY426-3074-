f72caa55ec768e1bbea2a750f62a32c34f2f4113
22bh22 <118910004+22bh22@users.noreply.github.com>
2023-09-23 03:32:29 +0000

    Update exercises.py
    works

--- traning_diary/exercises.py
+++ traning_diary/exercises.py
@@ -1,3 +1,4 @@
+#exercises.py
 class ExerciseInterface:
     
   def print_ex(self):
@@ -12,13 +13,13 @@ class ExerciseInterface:
 
 
 class PowerExercise(ExerciseInterface):
-  x_type = 'PowerExercise'
+  x_user_type = 'power'
   def __init__(self, name, weight, sets, reps, type):
     self.name = name
     self.weight = weight
     self.sets = sets
     self.reps = reps
-    self.x_type = type
+    self.x_user_type = type
 
   def __str__(self):
     return f"{self.name}: {self.weight} x {self.sets} x {self.reps}"
@@ -28,7 +29,7 @@ class PowerExercise(ExerciseInterface):
 
   def as_json(self):
     return {
-      'type': self.x_type,
+      'type': self.x_user_type,
       'name': self.name,
       'weight': self.weight,
       'sets': self.sets,
@@ -36,12 +37,12 @@ class PowerExercise(ExerciseInterface):
     }
   
 class CardioExercise(ExerciseInterface):
-  x_type = 'CardioExercise'
+  x_user_type = 'cardio'
   def __init__(self, name, distance, time, type):
     self.name = name
     self.x_distance = distance
     self.x_time = time
-    self.x_type = type
+    self.x_user_type = type
 
   def __str__(self):
     return f"{self.name}: {self.x_distance} x {self.x_time}"
@@ -51,8 +52,26 @@ class CardioExercise(ExerciseInterface):
 
   def as_json(self):
     return {
-      'type': self.x_type,
+      'type': self.x_user_type,
       'name': self.name,
       'distance': self.x_distance,
       'time': self.x_time
     }
+  
+  class CrossFit(ExerciseInterface):
+    """
+    Для кроссфита существует также подкласс упражнений:
+    'AFAP' - 
+    'AMRAP'
+    'EMOM'
+    'Chipper' - 
+    'Tabata' - 8 упражнений в режиме 20 секунд нагрузки, 10 секунд отдыха
+    'AMReps' - как можно больше повторений за определённое время
+    'Death by reps' - вес не меняется, количество повторений увеличивается
+    'Death by weight' - количество повторений не меняется, вес увеличивается
+    """
+    x_CF_ex_list = [
+      'AFAP', 'AMRAP', 'EMOM', 'Chipper', 
+      'Tabata', 'AMReps',
+      'Death by reps', 'Death by weight'
+      ]
